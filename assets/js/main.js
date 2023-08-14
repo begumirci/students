327d8026e17c7ed8b3f29895c4e76856b3fef3aa
Begüm <begumircci@outlook.com>
2023-08-14 04:21:14 +0300

    kadın erkek filtresi yapıldı

---
+++ assets/js/main.js
@@ -0,0 +1,134 @@
+const dialogElement = document.querySelector('dialog');
+const inputForm = document.querySelector('#studentİnfoForm');
+
+const dialogAc = document.querySelector('#ogrenciEkle');
+const allStudent = document.querySelector('#allStudent');
+
+const dialogKapat = document.querySelector('#vazgec');
+const class1 = document.querySelector('#sinif1');
+const class2 = document.querySelector('#sinif2');
+const class3 = document.querySelector('#sinif3');
+const class4 = document.querySelector('#sinif4');
+
+
+const yazilacakYer = document.querySelector('.burayaYaz');
+
+const options = document.querySelectorAll('[name="genders"]');
+
+
+const classrooms = [
+    class1,
+    class2,
+    class3,
+    class4
+]
+
+let students = [
+    {
+      
+    firstName:'Begüm',
+    lastName:'Ircı',
+    gender:'Kadın',
+    birthDate:'17.04.1999',
+    img:'https://picsum.photos/id/1/80/80',
+    classroom: 1
+    }
+]
+
+dialogAc.addEventListener('click',openDialog);
+dialogKapat.addEventListener('click',closeDialog);
+
+function openDialog(){
+    dialogElement.showModal();
+}
+
+function closeDialog(){
+    dialogElement.close();
+}
+
+let toplamOgrenci = 1;
+allStudent.innerText = toplamOgrenci;
+
+inputForm.addEventListener('submit',important);
+function important(){
+    const studentData = new FormData(inputForm);
+    const studentObj = Object.fromEntries(studentData.entries());
+    students.push(studentObj);
+    addStudent(studentObj);
+    inputForm.reset();
+    toplamOgrenci+=1;
+    allStudent.innerText = toplamOgrenci;
+    
+}
+
+function init(){
+    for (const student of students) {
+        addStudent(student);
+    }
+}
+
+function addStudent(student){
+    classrooms[student.classroom].appendChild(OgrenciOlustur(student));
+}
+
+function OgrenciOlustur(student){
+    const studentİnform = document.createElement('div');
+    studentİnform.classList.add('student');
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Sil';
+    studentİnform.appendChild(deleteBtn);
+
+    
+    studentİnform.innerHTML = `<div class= 'ogrenci-konum'>
+    <img src="${student.img}" alt="">
+    <div class= 'ogrenci-bilgi'>
+    <h6>Ad Soyad: ${student.firstName} ${student.lastName}</h6>
+    <h6>Cinsiyet: ${student.gender}</h6>
+    <h6>Doğum Tarihi: ${student.birthDate}</h6>
+    </div>
+    </div>`
+    deleteBtn.classList.add('delBtn')
+    deleteBtn.addEventListener('click',deleteStudent);
+    studentİnform.appendChild(deleteBtn);
+
+    return studentİnform;
+}
+
+for (const btn of options) {
+    btn.addEventListener('click',change);
+}
+
+function change(){
+    yazilacakYer.classList.value = 'burayaYaz ' + this.value;
+    yazilacakYer.innerText = '';
+    for (const student of students) {
+        if(yazilacakYer.classList.contains('Tümü')){
+            yazilacakYer.innerHTML += `<li>${student.firstName}</li>`
+            
+        }else if(yazilacakYer.classList.contains('Erkek')){
+
+            if(student.gender === 'Erkek'){
+                yazilacakYer.innerHTML += `<li>${student.firstName}</li>`
+            }
+        }else if(yazilacakYer.classList.contains('Kadın')){
+
+            if(student.gender === 'Kadın'){
+                yazilacakYer.innerHTML += `<li>${student.firstName}</li>`
+            }
+        }
+    }
+    
+}
+
+
+function deleteStudent(){
+    this.parentElement.remove();
+    toplamOgrenci -=1;
+     allStudent.innerText = toplamOgrenci;
+
+}
+
+init();
+
+deneme();
